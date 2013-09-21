d53f02dc86b4fd42e70a8fa565a556b44c32e601
shonenada <shonenada@gmail.com>
2013-09-21 23:50:48 +0800

    Fixed bugs.

--- classical-encryption/displacement.c
+++ classical-encryption/displacement.c
@@ -11,55 +11,50 @@ int main(){
     int key;
     char* proclaim;
     char* ciphertext;
+
     proclaim = (char*)malloc(sizeof(char) * 100);
     ciphertext = (char*)malloc(sizeof(char) * 100);
-    printf("Please input the key for encrypto the text:\n");
-    scanf("%d", &key);
+
     printf("Please input the proclaim:\n");
     scanf("%s", proclaim);
+    printf("Please input the key for encrypto the text:\n");
+    scanf("%d", &key);
     encrypto(proclaim, key);
     printf("Encrypto result: %s\n", proclaim);
-    printf("Please input the key for decrypto the text:\n");
-    scanf("%d", &key);
+
     printf("Please input the cipher text:\n");
     scanf("%s", ciphertext);
+    printf("Please input the key for decrypto the text:\n");
+    scanf("%d", &key);
     decrypto(ciphertext, key);
     printf("Decrypto result: %s\n", ciphertext);
+
+    getchar();getchar();
+
     free(proclaim);
     free(ciphertext);
+
     return 0;
-}
-
-char encrypto_char(char c, int key){
-    char output;
-
-    output = (c - 97 + key) % 26 + 97;
-    
-    return output;
 }
 
 void encrypto(char* input, int key){
     int i=0;
     while(input[i] != '\0'){
-        input[i] = encrypto_char(input[i], key);
+        input[i] = (input[i] - 97 + key) % 26 + 97;
         i = i + 1;
-    }
-}
-
-char decrypto_char(char c, int key){
-    char output;
-    output = c - 97 - key;
-    if (output >= 0){
-        return (output % 26 + 97);
-    }else{
-        return (output + 97 + 26);
     }
 }
 
 void decrypto(char * input, int key){
     int i=0;
+    key = key % 26;
     while(input[i] != '\0'){
-        input[i] = decrypto_char(input[i], key);
+        int temp = input[i] - 97 - key;
+        if (temp >= 0){
+            input[i] = temp % 26 + 97;
+        }else{
+            input[i] = input[i] + 26 - key;
+        }
         i = i + 1;
     }
 }
--- classical-encryption/vigenere.c
+++ classical-encryption/vigenere.c
@@ -10,28 +10,32 @@ int main(){
     char* key;
     char* proclaim;
     char* ciphertext;
+    char* encrypto_result;
+    char* decrypto_result;
 
     key = (char*)malloc(sizeof(char) * 100);
     proclaim = (char*)malloc(sizeof(char) * 100);
     ciphertext = (char*)malloc(sizeof(char) * 100);
 
-    printf("Please input the key for encrypto the text:\n");
-    scanf("%s", key);
     printf("Please input the proclaim:\n");
     scanf("%s", proclaim);
-    proclaim = encrypto(proclaim, key);
-    printf("Result: %s\n", proclaim);
-
-    printf("Please input the key for decrypto the text:\n");
+    printf("Please input the key for encrypto the text:\n");
     scanf("%s", key);
+    encrypto_result = encrypto(proclaim, key);
+    printf("Result: %s\n", encrypto_result);
+
     printf("Please input the text:\n");
     scanf("%s", ciphertext);
-    ciphertext = decrypto(ciphertext, key);
-    printf("Result: %s\n", ciphertext);
+    printf("Please input the key for decrypto the text:\n");
+    scanf("%s", key);
+    decrypto_result = decrypto(ciphertext, key);
+    printf("Result: %s\n", decrypto_result);
 
     free(key);
     free(proclaim);
     free(ciphertext);
+    free(encrypto_result);
+    free(decrypto_result);
 
     return 0;
 }
@@ -52,21 +56,23 @@ char* encrypto(char* input, char *key){
     int ptlen = str_len(input);
 
     int quotient = ptlen / key_len;
-    int remainder = ptlen % key_len;
+    int rest = ptlen % key_len;
 
     char* output = (char*)malloc(sizeof(char) * 100);
     for (i=0;i<quotient;++i){
         for (j=0;j<key_len; ++j){
-            int c = (input[i * key_len + j] - 97 + key[j] - 97) % 26 +'a';
+            int c = (input[i * key_len + j] - 97 + key[j] - 97) % 26 + 'a';
             output[index] = c;
             index ++;
         }
     }
-    for (i=0;i<remainder; ++i){
+    for (i=0;i<rest; ++i){
         int c = (input[quotient * key_len + i] - 97 + key[i] - 97) % 26 + 97;
         output[index] = c;
         index ++;
     }
+
+    output[ptlen] = '\0';
 
     return output;
     
@@ -81,7 +87,7 @@ char* decrypto(char* input, char* key){
     int ptlen = str_len(input);
 
     int quotient = ptlen / key_len;
-    int remainder = ptlen % key_len;
+    int rest = ptlen % key_len;
 
     char* output = (char*)malloc(sizeof(char) * 100);
     for(i=0;i<quotient;++i){
@@ -91,10 +97,13 @@ char* decrypto(char* input, char* key){
             index ++;
         }
     }
-    for(i=0;i<remainder; ++i){
+    for(i=0;i<rest; ++i){
         int c = (input[quotient * key_len + i] - 97 + 26 - (key[i] - 97)) % 26 + 97;
         output[index] = c;
         index ++;
     }
+    
+    output[ptlen] = '\0';
+
     return output;
 }
